85ab7539aa1c55ef25dc3887eaa6890f892a28f9
[dyad] <pastorinimatheus@gmail.com>
2025-08-31 23:23:40 -0300

    [dyad] wrote 5 file(s), executed 2 SQL queries

--- src/pages/admin/Settings.tsx
+++ src/pages/admin/Settings.tsx
@@ -291,9 +291,8 @@ const SettingsPage: React.FC = () => {
         </CardHeader>
         <CardContent>
           <div className="flex items-center mb-2">
-            <Label htmlFor="system_prompt" className="sr-only">Prompt do Sistema</Label>
             <FieldInsertPopover fields={userDataFields} onInsert={handleInsertSystemPromptField} />
-            <FieldInsertPopover fields={powers} onInsert={handleInsertSystemPromptPower} /> {/* Novo popover para poderes */}
+            <FieldInsertPopover fields={powers} onInsert={handleInsertSystemPromptPower} />
           </div>
           <Controller
             control={control}
@@ -323,9 +322,8 @@ const SettingsPage: React.FC = () => {
         </CardHeader>
         <CardContent>
           <div className="flex items-center mb-2">
-            <Label htmlFor="assistant_prompt" className="sr-only">Prompt do Assistente</Label>
             <FieldInsertPopover fields={userDataFields} onInsert={handleInsertAssistantPromptField} />
-            <FieldInsertPopover fields={powers} onInsert={handleInsertAssistantPromptPower} /> {/* Novo popover para poderes */}
+            <FieldInsertPopover fields={powers} onInsert={handleInsertAssistantPromptPower} />
           </div>
           <Controller
             control={control}
