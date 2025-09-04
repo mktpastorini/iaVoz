dcf7642b416e1dc8e92559e891452e5e2b036ab7
[dyad] <pastorinimatheus@gmail.com>
2025-09-04 00:55:11 -0300

    [dyad] Corrigindo erro crítico do React com a configuração 'dedupe' do Vite. - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -19,11 +19,7 @@ export default defineConfig(() => ({
   resolve: { 
     alias: { 
       "@": path.resolve(__dirname, "./src"),
-      "react": path.resolve(__dirname, "./node_modules/react"),
-      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
-      "@react-three/fiber": path.resolve(__dirname, "./node_modules/@react-three/fiber"),
-      "@react-three/postprocessing": path.resolve(__dirname, "./node_modules/@react-three/postprocessing"),
-      "three": path.resolve(__dirname, "./node_modules/three"),
-    }, 
+    },
+    dedupe: ['react', 'react-dom'],
   },
 }));
