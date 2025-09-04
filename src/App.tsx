d4db3569968fe376a5871ea1ef8f14637bc79ebe
[dyad] <pastorinimatheus@gmail.com>
2025-09-04 00:07:31 -0300

    [dyad] Removing an unused notification system to fix a recurring error. - wrote 1 file(s), deleted 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -95,7 +94,6 @@ const GlobalVoiceAssistantWrapper = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <BrowserRouter>
         <SessionContextProvider>
