0a1ea48dbef6e311d55ac4b6ad0ef4777c7bc83e
[dyad] <pastorinimatheus@gmail.com>
2025-09-06 02:20:32 -0300

    [dyad] Fixed React rendering error by refactoring toast hook - wrote 1 file(s)

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -87,8 +87,6 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
@@ -123,14 +121,14 @@ export const reducer = (state: State, action: Action): State => {
   }
 };
 
-const listeners: Array<(state: State) => void> = [];
+const listeners: Array<() => void> = [];
 
 let memoryState: State = { toasts: [] };
 
 function dispatch(action: Action) {
   memoryState = reducer(memoryState, action);
   listeners.forEach((listener) => {
-    listener(memoryState);
+    listener();
   });
 }
 
@@ -166,17 +164,18 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState);
-
-  React.useEffect(() => {
-    listeners.push(setState);
-    return () => {
-      const index = listeners.indexOf(setState);
-      if (index > -1) {
-        listeners.splice(index, 1);
-      }
-    };
-  }, []);
+  const state = React.useSyncExternalStore(
+    (onStoreChange) => {
+      listeners.push(onStoreChange);
+      return () => {
+        const index = listeners.indexOf(onStoreChange);
+        if (index > -1) {
+          listeners.splice(index, 1);
+        }
+      };
+    },
+    () => memoryState
+  );
 
   return {
     ...state,
